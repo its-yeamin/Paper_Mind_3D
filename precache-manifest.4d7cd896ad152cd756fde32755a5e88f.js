a09b5a45e83584adc49050d55809e008a930dfc1
its-yeamin <alye@itu.dk>
2026-06-10 21:53:04 +0200

    Updates

---
+++ precache-manifest.4d7cd896ad152cd756fde32755a5e88f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a61afdc7a45d5a6287f2995b78d948f",
+    "url": "/Paper_Mind_3D/asaro.glb"
+  },
+  {
+    "revision": "9fe56c1a4d91d5a370d1",
+    "url": "/Paper_Mind_3D/css/app.0ffc71c0.css"
+  },
+  {
+    "revision": "2baf27e1f02dc0dc756495e2f1645f24",
+    "url": "/Paper_Mind_3D/img/Canvas-Cube.2baf27e1.svg"
+  },
+  {
+    "revision": "d4f6d157f9aeaa2aa0d3c2a406df3f70",
+    "url": "/Paper_Mind_3D/img/Canvas-Cylinder.d4f6d157.svg"
+  },
+  {
+    "revision": "c968c8cbd14dbd9e9774e9d3adb4064e",
+    "url": "/Paper_Mind_3D/img/Canvas-Head.c968c8cb.svg"
+  },
+  {
+    "revision": "7aaa175ca9a507313103bf094fe93886",
+    "url": "/Paper_Mind_3D/img/Canvas-Plane.7aaa175c.svg"
+  },
+  {
+    "revision": "b50acb480c5ccfa3499ec2a1722195bf",
+    "url": "/Paper_Mind_3D/img/Canvas-Sphere.b50acb48.svg"
+  },
+  {
+    "revision": "8b6adbf423ecdeb44d944b749c366289",
+    "url": "/Paper_Mind_3D/img/Dropdown.8b6adbf4.svg"
+  },
+  {
+    "revision": "c0870bd8d10da7d6f2573821978028c3",
+    "url": "/Paper_Mind_3D/img/View_in_AR.c0870bd8.svg"
+  },
+  {
+    "revision": "6e755bebc53fcbd9664e9a8b747f324c",
+    "url": "/Paper_Mind_3D/img/checkbox-checked.6e755beb.svg"
+  },
+  {
+    "revision": "0e2cd6b8998b36f9550565301dece967",
+    "url": "/Paper_Mind_3D/img/checkbox.0e2cd6b8.svg"
+  },
+  {
+    "revision": "4c889d146eed7d1a66b53f92a7e1c886",
+    "url": "/Paper_Mind_3D/img/draw.4c889d14.svg"
+  },
+  {
+    "revision": "b2547e18d8034485918f7cdff57bfa8e",
+    "url": "/Paper_Mind_3D/img/erase.b2547e18.svg"
+  },
+  {
+    "revision": "ce55312102950fbe9eac9c885395727c",
+    "url": "/Paper_Mind_3D/img/hideCanvas.ce553121.svg"
+  },
+  {
+    "revision": "70bd3527c5704081ed6d602b10123b34",
+    "url": "/Paper_Mind_3D/img/lockControls.70bd3527.svg"
+  },
+  {
+    "revision": "dae4705252bcba8dac3b22334605472b",
+    "url": "/Paper_Mind_3D/img/mesh.jpg"
+  },
+  {
+    "revision": "b111d562af54e2bcd38185947c52726a",
+    "url": "/Paper_Mind_3D/img/redo.b111d562.svg"
+  },
+  {
+    "revision": "df02008a2b1725c36f2ceee7a0725510",
+    "url": "/Paper_Mind_3D/img/reset.df02008a.svg"
+  },
+  {
+    "revision": "1fac32455eabb9d42713ebab61fa868e",
+    "url": "/Paper_Mind_3D/img/scale.1fac3245.svg"
+  },
+  {
+    "revision": "cc0b0c0b068289205fc949d9f8a7273e",
+    "url": "/Paper_Mind_3D/img/share.cc0b0c0b.svg"
+  },
+  {
+    "revision": "a80dc4da476c693abb398d588cbd3fcf",
+    "url": "/Paper_Mind_3D/img/showCanvas.a80dc4da.svg"
+  },
+  {
+    "revision": "80595415246af153e82d277d669d3d62",
+    "url": "/Paper_Mind_3D/img/snapOff.80595415.svg"
+  },
+  {
+    "revision": "4f3fcb1dffba0b9f37bf2bf4a2752eea",
+    "url": "/Paper_Mind_3D/img/snapOn.4f3fcb1d.svg"
+  },
+  {
+    "revision": "7fafbd4a98cd4f2b7c27d6a35cd9f093",
+    "url": "/Paper_Mind_3D/img/translate.7fafbd4a.svg"
+  },
+  {
+    "revision": "27d64ee450ad277b1e7a88cdbe8dd4e8",
+    "url": "/Paper_Mind_3D/img/undo.27d64ee4.svg"
+  },
+  {
+    "revision": "4414c97589cb3845786dd7c627a38e67",
+    "url": "/Paper_Mind_3D/img/unlockControls.4414c975.svg"
+  },
+  {
+    "revision": "e53a58b2461797b8283dd09adebe7c73",
+    "url": "/Paper_Mind_3D/img/watercolor.jpg"
+  },
+  {
+    "revision": "9b6b0dd22e0c88f58fa34d98d50274d6",
+    "url": "/Paper_Mind_3D/index.html"
+  },
+  {
+    "revision": "9fe56c1a4d91d5a370d1",
+    "url": "/Paper_Mind_3D/js/app.4ae5b483.js"
+  },
+  {
+    "revision": "c7742f4397d7d6e21aa1",
+    "url": "/Paper_Mind_3D/js/chunk-vendors.4f6c3ee8.js"
+  },
+  {
+    "revision": "d0eacf010e76fbd362c3a14c876d77ab",
+    "url": "/Paper_Mind_3D/manifest.json"
+  },
+  {
+    "revision": "3d425796b12e6eae16931f830a77dd69",
+    "url": "/Paper_Mind_3D/penzil.png"
+  },
+  {
+    "revision": "3c21b0842b1c0710636b2f29fea3422f",
+    "url": "/Paper_Mind_3D/penzil_preview.png"
+  },
+  {
+    "revision": "7b1a12a07586982fdd111be569dbaec1",
+    "url": "/Paper_Mind_3D/reset.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Paper_Mind_3D/robots.txt"
+  }
+]);
